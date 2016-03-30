5b425fca06e84523b077f94a0b8506a830ff0dc7
Lucas Unnerfelt <llasarus@gmail.com>
2016-03-30 20:07:09 +0200

    fixed install script

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 name=$1
 home="/home/$1"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # install x configuration files
-ln -s xinitrc "$home/.xinitrc"
-ln -s Xdefaults "$home/.Xdefaults"
+ln -nfs "$DIR/xinitrc" "$home/.xinitrc"
+ln -nfs "$DIR/Xdefaults" "$home/.Xdefaults"
 
 # install xmonad
 mkdir -p "$home/.xmonad"
-ln -s xmonad.hs "$home/.xmonad/xmonad.hs"
+ln -nfs "$DIR/xmonad.hs" "$home/.xmonad/xmonad.hs"
 
 # install emacs
-ln -s .emacs.d "$home/.emacs.d"
+ln -nfs "$DIR/.emacs.d" "$home/.emacs.d"
